56c17970f6281c878f164e67700b934ed3bee6ec
Bruno Santos <bmrosantos@gmail.com>
2018-10-17 21:28:47 -0400

    Minor updates

--- installers/nuget_linux_installer.sh
+++ installers/nuget_linux_installer.sh
@@ -5,7 +5,7 @@ sudo curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline
 DEST=/usr/local/bin/nuget
 
 sudo touch $DEST
-sudo echo "#!/usr/bin/env bash" > $DEST
-sudo echo "mono /usr/local/bin/nuget.exe \"$@\"" >> $DEST
 sudo chmod 755 $DEST
 sudo chown root:root $DEST
+sudo echo "#!/usr/bin/env bash" > $DEST
+sudo echo "mono /usr/local/bin/nuget.exe \"$@\"" >> $DEST
