09da5855f01703849defb38cf70e6f940f61f148
Bruno Santos <bmrosantos@gmail.com>
2018-10-17 21:26:20 -0400

    Create dest file

--- installers/nuget_linux_installer.sh
+++ installers/nuget_linux_installer.sh
@@ -4,6 +4,7 @@ sudo curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline
 
 DEST=/usr/local/bin/nuget
 
+sudo touch $DEST
 sudo echo "#!/usr/bin/env bash" > $DEST
 sudo echo "mono /usr/local/bin/nuget.exe \"$@\"" >> $DEST
 sudo chmod 755 $DEST
